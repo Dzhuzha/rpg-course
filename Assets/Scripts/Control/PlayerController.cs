3ed6591d5dbf08215225f16f18774b5539a77b3f
r.dzhuzha <dzhuzha.roman@gmail.com>
2023-07-10 15:57:13 +0300

    Added distance check logic to pointer position on nav mesh to handle cursor icon and max nav mesh calculation distance

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -3,6 +3,7 @@ using RPG.Atributes;
 using RPG.Combat;
 using RPG.Movement;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 namespace RPG.Control
@@ -12,6 +13,8 @@ namespace RPG.Control
         [SerializeField] private Camera _camera;
         [SerializeField] private Mover _mover;
         [SerializeField] private Fighter _fighter;
+        [SerializeField] private float _maxNavMeshProjectionDistance = 1f;
+        [SerializeField] private float _maxNavMeshPathLength = 5f;
         [SerializeField] private EnemyHealthDisplay _enemyHealthDisplay;
         [NonReorderable, SerializeField] private CursorMapping[] _cursorMappings;
         
@@ -48,7 +51,7 @@ namespace RPG.Control
 
         private bool InteractWithComponent()
         {
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            RaycastHit[] hits = RaycastSorted();
 
             foreach (RaycastHit hit in hits)
             {
@@ -66,6 +69,20 @@ namespace RPG.Control
             return false;
         }
 
+        private RaycastHit[] RaycastSorted()
+        {
+            RaycastHit[] unsortedHits = Physics.RaycastAll(GetMouseRay());
+            float[] distances = new float [unsortedHits.Length];
+            
+            for (int i = 0; i < unsortedHits.Length; i++)
+            {
+                distances[i] = unsortedHits[i].distance;
+            }
+            
+            Array.Sort(distances, unsortedHits);
+            return unsortedHits;
+        }
+
         private bool InteractWithUI()
         {
             if (!EventSystem.current.IsPointerOverGameObject()) return false;
@@ -81,21 +98,52 @@ namespace RPG.Control
 
         private bool InteractWithMovement()
         {
-            if (Physics.Raycast(GetMouseRay(), out RaycastHit hit))
+            if (!IsMovable(out Vector3 targetPosition)) return false;
+            
+            if (Input.GetMouseButton(0))
             {
-                if (Input.GetMouseButton(0))
-                {
-                    _mover.StartMoveAction(hit.point);
-                    _enemyHealthDisplay.ResetTarget();
-                }
-
-                SetCursor(CursorType.Movement);
-                return true;
+                _mover.StartMoveAction(targetPosition);
+                _enemyHealthDisplay.ResetTarget();
             }
-
-            return false;
+            
+            SetCursor(CursorType.Movement);
+            return true;
         }
-        
+
+        private bool IsMovable(out Vector3 targetPosition)
+        {     
+            targetPosition = new Vector3();
+            if (!Physics.Raycast(GetMouseRay(), out RaycastHit hit)) return false;
+            
+            bool hasCastToNavMesh = NavMesh.SamplePosition(hit.point, out NavMeshHit navMeshHit, 
+                _maxNavMeshProjectionDistance, NavMesh.AllAreas);
+
+            if (hasCastToNavMesh)
+            {
+                targetPosition = navMeshHit.position;
+            }
+            
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
+            if (!hasPath || path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength(path) > _maxNavMeshPathLength) return false;
+
+            return hasCastToNavMesh;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float totalDistance = 0f;
+            if (path.corners.Length < 2) return totalDistance;
+
+            for (int i = 1; i < path.corners.Length; i++)
+            {
+                totalDistance += Vector3.Distance(path.corners[i-1], path.corners[i]);
+            }
+            
+            return totalDistance;
+        }
+
         private CursorMapping GetCursorMapping(CursorType cursorType)
         {
             foreach (var mapping in _cursorMappings)
