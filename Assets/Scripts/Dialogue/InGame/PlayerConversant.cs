87ad01029c322528bfb076ce31cdff4c2797d22c
r.dzhuzha <dzhuzha.roman@gmail.com>
2023-08-03 19:04:49 +0300

    Added simple changing of the next dialogue replique logic. Added close dialogue button logic

--- Assets/Scripts/Dialogue/InGame/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/InGame/PlayerConversant.cs
@@ -1,15 +1,47 @@
 using System;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace RPG.Dialogue
 {
     public class PlayerConversant : MonoBehaviour
     {
         [SerializeField] private Dialogue _currentDialogue;
+        private DialogueNode _currentNode;
+
+        private void Awake()
+        {
+            _currentNode = _currentDialogue.GetRootNode();
+        }
 
         public string GetText()
         {
-            return _currentDialogue == null ? String.Empty : _currentDialogue.GetRootNode().Text;
+            if (_currentNode == null)
+            {
+                return String.Empty;
+            }
+
+
+            return _currentNode.Text;
+        }
+
+        public bool TryGetNext()
+        {
+            return _currentNode.Children.Count > 0;
+        }
+
+        public void ChooseNextNode()
+        {
+            DialogueNode[] children = _currentDialogue.GetAllChildren(_currentNode).ToArray();
+            int npcAnswerOption = 0;
+            
+            if (!_currentNode.IsPlayerSpeaking)
+            {
+                npcAnswerOption = Random.Range(0, children.Length - 1);
+            }
+
+            _currentNode = children[npcAnswerOption];
         }
     }
 }
--- Assets/Scripts/UI/Dialogue/DialogueUI.cs
+++ Assets/Scripts/UI/Dialogue/DialogueUI.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using RPG.Dialogue;
 using TMPro;
+using UnityEngine.UI;
 
 namespace RPG.UI
 {
     public class DialogueUI : MonoBehaviour
     {
+        [SerializeField] private Button _continueButton;
+        [SerializeField] private Button _closeButton;
         [SerializeField] private TMP_Text _text;
         private PlayerConversant _playerConversant;
 
@@ -14,9 +17,38 @@ namespace RPG.UI
             _playerConversant = FindObjectOfType<PlayerConversant>();
         }
 
+        private void OnEnable()
+        {
+            _closeButton.onClick.AddListener(Close);
+            _continueButton.onClick.AddListener(Next);
+        }
+
+        private void OnDisable()
+        {
+            _closeButton.onClick.AddListener(Close);
+            _continueButton.onClick.RemoveListener(Next);
+        }
+
         private void Start()
         {
+            UpdateUI();
+        }
+
+        private void Next()
+        {
+            _playerConversant.ChooseNextNode();
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            _continueButton.gameObject.SetActive(_playerConversant.TryGetNext());
             _text.text = _playerConversant.GetText();
+        }
+
+        private void Close()
+        {
+            gameObject.SetActive(false);
         }
     }
 }
