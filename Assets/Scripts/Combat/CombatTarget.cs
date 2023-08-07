c883e6823f6dee8be7dd1aaff1e71d40221ebcec
r.dzhuzha <dzhuzha.roman@gmail.com>
2023-08-07 15:06:16 +0300

    Added mob aggro group to switch NPC behaviour

--- Assets/Scripts/Combat/CombatTarget.cs
+++ Assets/Scripts/Combat/CombatTarget.cs
@@ -7,17 +7,10 @@ namespace RPG.Combat
     [RequireComponent(typeof(Atributes.Health))]
     public class CombatTarget : MonoBehaviour, IRaycastable
     {
-        private Fighter _fighterComponent;
-        
-        private void Awake()
-        {
-            _fighterComponent = GetComponent<Fighter>();
-        }
-
         public bool HandleRaycast(PlayerController callingController)
         {
             if (!callingController.transform.TryGetComponent(out Fighter fighter)) return false;
-            if (!_fighterComponent.enabled) return false;
+            if (!enabled) return false;
 
             if (Input.GetMouseButton(0) && fighter.CanAttack(gameObject))
             {
@@ -30,8 +23,6 @@ namespace RPG.Combat
 
         public CursorType GetCursorType()
         {
-            if (!_fighterComponent.enabled) return CursorType.None;
-  
             return CursorType.Combat;
         }
     }
